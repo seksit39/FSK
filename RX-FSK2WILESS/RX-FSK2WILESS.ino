e6e677e797664ab69c9551e7d0788228484d6542
seksit39 <seksit39@outlook.com>
2017-02-20 10:39:02 +0700

    Add files via upload

---
+++ RX-FSK2WILESS/RX-FSK2WILESS.ino
@@ -0,0 +1,84 @@
+// Recieve FSK
+
+
+int input,max,prev,currentTime,prevTime,period,cout = 0,totalTime = 0;
+float currentFrequency,prevFrequency;
+boolean check = false;
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))             // this for increase analogRead Speed
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+int c00 = 0,                                                    // for count number of data
+    c01 = 0,
+    c10 = 0,
+    c11 = 0;
+
+void setup(void) 
+{
+  sbi(ADCSRA,ADPS2) ;                                           // this for increase analogRead Speed
+  cbi(ADCSRA,ADPS1) ;
+  cbi(ADCSRA,ADPS0) ;
+  Serial.begin(115200);                                         // set buadrate of serial
+}
+
+void loop(void) 
+{   
+    input = analogRead(A0);                                     // read signal from pin A0
+    
+    if(input-prev>30 && check==false){                          // if signal is rising
+      max = 0;                                                  // change max value to zero
+      check = true;                                             // assign check is true
+    }
+    if(input>max){                                              // set max input
+      max=input;
+    }  
+    if(max-input > 50 && check == true){                        // if signal is amplitude
+      currentTime = micros();                                   // get time from in microsecond
+      period =  currentTime - prevTime;                         // calculate period
+      currentFrequency = 1000000/period;                        // calculate frequency
+      prevFrequency = currentFrequency;
+      prevTime = currentTime;
+      
+      //Serial.println(currentFrequency);                       // for debug frequency
+
+      if(1125 < currentFrequency && currentFrequency <= 1375)   // if signal frequency is between 1125 to 1375 Hz
+         c11++;                                                 // count up c11
+      if(875 < currentFrequency && currentFrequency <= 1125)    // if signal frequency is between 875 to 1125 Hz
+         c10++;
+      if(625 < currentFrequency && currentFrequency <= 875)     // if signal frequency is between 625 to 875 Hz
+         c01++;  
+      if(375 < currentFrequency && currentFrequency <= 625)     // if signal frequency is between 375 to 625 Hz
+         c00++;   
+            
+      if(c11 >= 5 || c10 >= 4 ||  c01 >= 3 || c00 >= 2){        // display data
+        if(c11 == 5)                                            // if c11 = 5 
+          Serial.print("1 1 ");                                 // print data or do other
+        if(c10 == 4)
+          Serial.print("1 0 ");                                 // print data
+        if(c01 == 3)
+          Serial.print("0 1 ");                                 // print data
+        if(c00 == 2)
+          Serial.print("0 0 ");                                 // print data 
+          
+        prevTime = 3000;                                        // set previous time for skip fuction count data
+        c11 = 0;
+        c10 = 0;
+        c01 = 0;
+        c00 = 0;
+        cout++;
+      }
+        
+      if(cout == 5){                                            // if print data for 5 time
+        Serial.println();                                       // send /n to serial
+        cout = 0;                                               // change count is zero 
+      } 
+      
+      check = false;                                            // change statement for do this 1 time per a signal
+    } 
+    prev = input;
+}
+
---
+++ TX-FSK2WILESS/TX-FSK2WILESS.ino
@@ -0,0 +1,96 @@
+/*
+sine delay
+none       1736 Hz
+20  micros 1500 Hz
+50  micros 1250 Hz
+100 micros 1000 Hz
+190 micros 750  Hz
+350 micros 500  Hz
+*/
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+#include <Adafruit_ADS1015.h>
+
+  #define defaultFreq 1700
+  #define freq0 500
+  #define freq1 750
+  #define freq2 1000
+  #define freq3 1250
+
+  
+  uint16_t a = 0;
+  uint16_t input[4] = {0,0,0,0};
+  const uint16_t sine[4]  = {2048,4095,2048,0};   // At 1700 Hz     
+  int del = 0;
+  int c00 = 0;
+  int c01 = 0;
+  int c10 = 0;
+  int c11 = 0;
+  int delay0,delay1,delay2,delay3;
+  
+  Adafruit_MCP4725 dac;
+  
+void setup(void) {
+  
+  Serial.begin(115200);                         // set buadrate serial
+  dac.begin(0x62);                              // set DAC default 
+
+  delay0 = (1000000/freq0 - 1000000/defaultFreq)/4;   // caculate period/4 of sinewave
+  delay1 = (1000000/freq1 - 1000000/defaultFreq)/4;
+  delay2 = (1000000/freq2 - 1000000/defaultFreq)/4;
+  delay3 = (1000000/freq3 - 1000000/defaultFreq)/4;
+  
+}
+
+void loop(void) {
+
+      if(Serial.available() > 0){               // for get input
+
+        int in = Serial.parseInt();             // get Dec from Serial
+        for(int i=0 ; i<4 ; i++){               // 4 time of loop for a byte input
+          input[i] = in & 3;                    // and bit b0000 0011 and save to array
+          in >>= 2;                             // shift bit right 2 bit
+        }
+        
+        //for(int i=0 ; i<10 ; i++)
+        for(int k=3 ; k>=0 ; k--){
+          
+            if(input[k] == 0){                    // for input 00 - 500 Hz
+                //Serial.println("out 00");
+                for(int j=0 ; j<2 ; j++)
+                  for(int i=0 ; i<4 ; i++){
+                    dac.setVoltage(sine[i], false);
+                    delayMicroseconds(delay0);    //360
+                  }
+             }
+              
+             else if(input[k] == 1){              // for input 01 - 750 Hz
+                //Serial.println("out 01");
+                for(int j=0 ; j<3 ; j++)
+                  for(int i=0 ; i<4 ; i++){
+                    dac.setVoltage(sine[i], false);
+                    delayMicroseconds(delay1);    //190
+                   }                
+             }
+ 
+              else if(input[k] == 2){             // for input 10 - 1000 Hz
+                 //Serial.println("out 10");
+                 for(int j=0 ; j<4 ; j++)
+                    for(int i=0 ; i<4 ; i++){
+                      dac.setVoltage(sine[i], false);
+                      delayMicroseconds(delay2);  //100 
+                    }
+              }
+              
+              else if(input[k] == 3){            // for input 11 - 1250 Hz
+                 //Serial.println("out 11");
+                 for(int j=0 ; j<5 ; j++)      
+                    for(int i=0 ; i<4 ; i++){
+                      dac.setVoltage(sine[i], false);
+                      delayMicroseconds(delay3); //50
+                    }             
+              }
+        } 
+      } 
+      dac.setVoltage(0, false);                 // for don't send
+}
